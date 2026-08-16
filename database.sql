9f8384719098455c006f13eec19af5868efcf63b
Jeroen-Janssens1 <jeroen-janssens@hotmail.com>
2026-08-16 17:41:04 +0200

    Added movies and their descriptions to the database (total of 11 examples)

--- database.sql
+++ database.sql
@@ -137,3 +137,17 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+INSERT INTO movies (movieName, movieDescription)
+VALUES
+ ("Harry Potter and the Philosopher's Stone", "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world."),
+("Harry Potter and the Chamber of Secrets", "Harry Potter lives his second year at Hogwarts with Ron and Hermione when a message on the wall announces that the legendary Chamber of Secrets has been opened. The trio soon realize that, to save the school, it will take a lot of courage."),
+("Harry Potter and the Prisoner of Azkaban", "Harry Potter, Ron and Hermione return to Hogwarts School of Witchcraft and Wizardry for their third year of study, where they delve into the mystery surrounding an escaped prisoner who poses a dangerous threat to the young wizard."),
+("Harry Potter and the Goblet of Fire", "Harry Potter finds himself competing in a hazardous tournament between rival schools of magic, but he is distracted by recurring nightmares."),
+("Harry Potter and the Order of the Phoenix", "With their warning about Lord Voldemort's return scoffed at, Harry and Dumbledore are targeted by the Wizard authorities as an authoritarian bureaucrat slowly seizes power at Hogwarts."),
+("Harry Potter and the Half-Blood Prince", "As Harry Potter begins his sixth year at Hogwarts, he discovers an old book marked as 'the property of the Half-Blood Prince' and begins to learn more about Lord Voldemort's dark past."),
+("Harry Potter and the Deathly Hallows: Part 1", "Harry Potter is tasked with the dangerous and seemingly impossible task of locating and destroying Voldemort's remaining Horcruxes. Harry must rely on Ron and Hermione more than ever, but dark forces threaten to tear them apart."),
+("Harry Potter and the Deathly Hallows: Part 2", "Harry, Ron and Hermione set out on a quest to eliminate the remaining horcruxes. On the other hand, the students and teachers must unite to defend Hogwarts against Lord Voldemort and his army."),
+("The Lord of the Rings: The Fellowship of the Ring", "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron."),
+("The Lord of the Rings: The Two Towers", "While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Sauron's new ally, Saruman, and his hordes of Isengard."),
+("The Lord of the Rings: The Return of the King", "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.");
